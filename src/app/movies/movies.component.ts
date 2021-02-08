c35dc364ef6283f84dd1822c17490e124a553c7f
Emre Durmayaz <emredurmayaz@hotmail.com>
2021-02-08 17:17:26 +0300

    Lesson 3: Component_Styles

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'movies' , //<movies><movies>   
-    templateUrl: 'movies.component.html'
+    templateUrl: 'movies.component.html',
+    styles: [`
+        h2{
+            color: blue;
+        }
+    `]
+
 
 })
 export class MoviesComponent{
