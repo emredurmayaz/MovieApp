edd051cf041d9e02ef3ea5c263913bdf97ab1588
Emre Durmayaz <emredurmayaz@hotmail.com>
2021-02-12 00:36:42 +0300

    Lesson 10: Dashboard

--- src/app/movies/movie.datasource.ts
+++ src/app/movies/movie.datasource.ts
@@ -3,10 +3,10 @@ import { Movie } from '../movie';
 
 
 export const Movies: Movie[] = [
-        {id:1, name:"movie 1"},
-        {id:2, name:"movie 2"},
-        {id:3, name:"movie 3"},
-        {id:4, name:"movie 4"},
-        {id:5, name:"movie 5"},
+        {id:1, name:"movie 1",description:"Güzel film",imageUrl:"1.jpg"},
+        {id:2, name:"movie 2",description:"Dandik film",imageUrl:"2.jpg"},
+        {id:3, name:"movie 3",description:"Tırt film",imageUrl:"3.jpg"},
+        {id:4, name:"movie 4",description:"Ceviz film",imageUrl:"4.jpg"},
+        {id:5, name:"movie 5",description:"Ala film",imageUrl:"5.jpg"},
         
 ];
